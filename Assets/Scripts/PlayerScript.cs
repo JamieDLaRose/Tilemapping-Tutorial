63943d1f8c1792e21e37faa4d02afb0904eb7b2e
JamieDLaRose <52182231+JamieDLaRose@users.noreply.github.com>
2020-04-27 12:35:56 -0400

    Challenge 2

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,31 +6,78 @@ using UnityEngine.UI;
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
-    public float speed;
-    public Text score;
 
-    private int scoreValue = 0;
+    public float speed;
+
+    public Text score;
+    public Text win;
+    public Text lives;
+
+    private int livesValue;
+    private int scoreValue;
+
+    private bool facingRight;
+
+    public AudioSource musicSource;
+    public AudioSource playerSounds;
+
+    public AudioClip backgroundMusic;
+    public AudioClip victorySound;
+
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        livesValue = 3;
+        scoreValue = 0;
+        facingRight = true;
+        win.text = "";
+        lives.text = "";
+        SetScore();
+        SetLives();
+        musicSource.loop = true;
+        playerSounds.loop = false;
+        musicSource.clip = backgroundMusic;
+        playerSounds.clip = victorySound;
+        musicSource.Play();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        float hozMovement = Input.GetAxis("Horizontal");
-        float vertMovement = Input.GetAxis("Vertical");
-
-        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
-
-        if (Input.GetKey("escape"))
+        //Player Flipping
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
-            Application.Quit();
+            if (Input.GetAxisRaw("Horizontal") > 0.5f && !facingRight)
+            {
+                Flip();
+                facingRight = true;
+            }
+            else if (Input.GetAxisRaw("Horizontal") < 0.5f && facingRight)
+            {
+                Flip();
+                facingRight = false;
+            }
         }
 
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 2);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -38,10 +85,21 @@ public class PlayerScript : MonoBehaviour
         if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            SetScore();
             Destroy(collision.collider.gameObject);
         }
 
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+            {
+                anim.SetInteger("State", 1);
+            }
+            else
+            {
+                anim.SetInteger("State", 0);
+            }
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
@@ -52,8 +110,45 @@ public class PlayerScript : MonoBehaviour
             {
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
             }
+
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A))
+            {
+                anim.SetInteger("State", 1);
+            }
+
+            if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+            {
+                anim.SetInteger("State", 0);
+            }
         }
     }
 
+    void SetScore()
+    {
+        score.text = "Score: " + scoreValue.ToString();
+        if (scoreValue == 4 && livesValue > 0)
+        {
+            win.text = "You win! Game created by Jamie LaRose.";
+            playerSounds.Play();
+        }
+    }
 
+    void SetLives()
+    {
+        lives.text = "Lives: " + livesValue.ToString();
+        if (livesValue <= 0 && scoreValue < 4)
+        {
+            win.text = "You lose. Game created by Jamie LaRose.";
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector2 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
